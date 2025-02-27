7b08ccd365737a8ce8299f06dad07bd55af4a4ea
janakaedirisinghe <janakapradeepedirisinghe@gmail.com>
2025-02-27 09:21:40 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
+const tenantMiddleware = require('../middleware/tenantMiddleware');
 
 router.get('/google', authController.googleAuth);
 router.get('/google/callback', authController.googleAuthCallback);
-router.get('/me', authMiddleware, authController.getMe);
+router.get('/me', tenantMiddleware, authMiddleware, authController.getMe);
 
 module.exports = router;
